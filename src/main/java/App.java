90282f6679ebb9402f1a3f0703e1f17608fcb758
Ashley Sullins <ashley.sullins@gmail.com>
2015-08-21 12:22:19 -0700

    Add spark UI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,19 +12,48 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String,Object>();
       model.put("template", "templates/home.vtl");
+            
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    
+    post("/word", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/home.vtl");
+      String name = request.queryParams("newWord");
+      Word newWord = new Word(name);
+      model.put("word", name); 
+      model.put("words", Word.all());
 
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/results", (request, response) -> {
+    get("/word/:id", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/results.vtl");
-
-      //Variables that you'd like to call on each page go here
+      model.put("template", "templates/word.vtl");
+      Word name = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", name);
 
       return new ModelAndView(model, layout);
-
-
+    }, new VelocityTemplateEngine());
+    
+    post("/definition", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/word.vtl");
+      String information = request.queryParams("newDefinition");
+      Definition newDefinition = new Definition(information);
+      model.put("definition", information); 
+      model.put("definitions", Definition.all());
+      Word name = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", name);
+      
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    
+    get("/word/:id/definition/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/definition.vtl");
+      
+      return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
 
--- src/test/java/AppIntegrationTest.java
+++ src/test/java/AppIntegrationTest.java
@@ -17,7 +17,49 @@ public class AppIntegrationTest extends FluentTest {
 
   @Test
   public void rootTest() {
-      goTo("http://localhost:4567/");
-      //assertThat(pageSource()).contains("Add your website text here");
+  goTo("http://localhost:4567/");
+  assertThat(pageSource()).contains("Suburban Dictionary");
   }
+
+  // @Test
+  // public void categoryIsCreatedTest() {
+  //   goTo("http://localhost:4567/");
+  //   click("a", withText("Add a New Category"));
+  //   fill("#name").with("Household chores");
+  //   submit(".btn");
+  //   assertThat(pageSource()).contains("Your category has been saved.");
+  // }
+  // 
+  // @Test
+  // public void categoryIsDisplayedTest() {
+  //   goTo("http://localhost:4567/categories/new");
+  //   fill("#name").with("Household chores");
+  //   submit(".btn");
+  //   click("a", withText("View all"));
+  //   assertThat(pageSource()).contains("Household chores");
+  // }
+  // 
+  // @Test
+  // public void categoryTasksFormIsDisplayed() {
+  //   goTo("http://localhost:4567/categories/new");
+  //   fill("#name").with("Shopping");
+  //   submit(".btn");
+  //   click("a", withText("View all"));
+  //   click("a", withText("Shopping"));
+  //   click("a", withText("Add a new task"));
+  //   assertThat(pageSource()).contains("Add a task to Shopping");
+  // }
+  // 
+  // @Test
+  // public void tasksIsAddedAndDisplayed() {
+  //   goTo("http://localhost:4567/categories/new");
+  //   fill("#name").with("Banking");
+  //   submit(".btn");
+  //   click("a", withText("View all"));
+  //   click("a", withText("Banking"));
+  //   click("a", withText("Add a new task"));
+  //   fill("#description").with("Deposit paycheck");
+  //   submit(".btn");
+  //   assertThat(pageSource()).contains("Deposit paycheck");
+  // }
 }
