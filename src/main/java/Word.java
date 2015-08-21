132a139b3667fe80d147a5f68365e08ee0fb6d21
Ashley Sullins <ashley.sullins@gmail.com>
2015-08-21 09:30:30 -0700

    Add passing tests for Word object

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,12 +1,38 @@
+import java.util.ArrayList;
+
 public class Word {
   private String mName;
+  private static ArrayList<Word> occurence = new ArrayList<Word>();
+  private int mId;
 
   public Word(String name) {
     mName = name;
+    occurence.add(this);
+    mId = occurence.size();
   }
 
   public String getName() {
     return mName;
+  }
+
+  public int getId() {
+  return mId;
+  }
+
+  public static ArrayList<Word> all() {
+  return occurence;
+  }
+
+  public static Word find(int id) {
+    try {
+      return occurence.get(id - 1);
+    } catch (IndexOutOfBoundsException e) {
+      return null;
+    }
+  }
+
+  public static void clear() {
+    occurence.clear();
   }
 
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -2,6 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class WordTest {
+  
+  @Rule
+  public ClearRule clearRule = new ClearRule();
 
   @Test
   public void Word_instantiatesCorrectly_true() {
@@ -14,4 +17,38 @@ public class WordTest {
     Word myWord = new Word("Dictionary");
     assertEquals("Dictionary", myWord.getName());
   }
+
+  @Test
+  public void all_returnsAllInstancesOfWord_true() {
+    Word firstWord = new Word("Dictionary");
+    Word secondWord = new Word("Engineering");
+    assertTrue(Word.all().contains(firstWord));
+    assertTrue(Word.all().contains(secondWord));
+  }
+
+  @Test
+  public void newId_WordInstantiateWithAnID_true() {
+    Word myWord = new Word("Dictionary");
+    assertEquals(Word.all().size(), myWord.getId());
+  }
+
+  @Test
+  public void find_returnsWordWithSameId_secondWord() {
+    Word firstWord = new Word("Dictionary");
+    Word secondWord = new Word("Engineering");
+    assertEquals(Word.find(secondWord.getId()), secondWord);
+  }
+
+  @Test
+  public void find_returnsNullWhenNoWordFound_null() {
+    assertTrue(Word.find(999) == null);
+  }
+
+  @Test
+  public void clear_emptiesAllWordsFromArrayList() {
+    Word myWord = new Word("Dictionary");
+    Word.clear();
+    assertEquals(Word.all().size(), 0);
+  }
+
 }
