864e786847f6c6c9cbb91206e211074367a7e67c
Ashley Sullins <ashley.sullins@gmail.com>
2015-10-15 19:25:46 -0700

    Add gradle wrapper

--- build/scripts/Dictionary-java.bat
+++ build/scripts/Dictionary-java.bat
@@ -9,7 +9,7 @@
 if "%OS%"=="Windows_NT" setlocal
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and DICTIONARY_JAVA_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+set DEFAULT_JVM_OPTS="-javaagent:../lib/newrelic-agent-3.14.0.jar"
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
@@ -69,7 +69,7 @@ set CMD_LINE_ARGS=%$
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\Dictionary-java-1.0.jar;%APP_HOME%\lib\spark-core-2.1.jar;%APP_HOME%\lib\velocity-1.7.jar;%APP_HOME%\lib\slf4j-api-1.7.7.jar;%APP_HOME%\lib\jetty-util-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-security-9.0.2.v20130417.jar;%APP_HOME%\lib\slf4j-simple-1.7.7.jar;%APP_HOME%\lib\jetty-server-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-webapp-9.0.2.v20130417.jar;%APP_HOME%\lib\commons-collections-3.2.1.jar;%APP_HOME%\lib\commons-lang-2.4.jar;%APP_HOME%\lib\javax.servlet-3.0.0.v201112011016.jar;%APP_HOME%\lib\jetty-http-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-io-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-xml-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-servlet-9.0.2.v20130417.jar
+set CLASSPATH=%APP_HOME%\lib\Dictionary-java-1.0.jar;%APP_HOME%\lib\spark-core-2.1.jar;%APP_HOME%\lib\velocity-1.7.jar;%APP_HOME%\lib\newrelic-agent-3.14.0.jar;%APP_HOME%\lib\newrelic-api-3.14.0.jar;%APP_HOME%\lib\slf4j-api-1.7.7.jar;%APP_HOME%\lib\slf4j-simple-1.7.7.jar;%APP_HOME%\lib\jetty-server-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-webapp-9.0.2.v20130417.jar;%APP_HOME%\lib\commons-collections-3.2.1.jar;%APP_HOME%\lib\commons-lang-2.4.jar;%APP_HOME%\lib\javax.servlet-3.0.0.v201112011016.jar;%APP_HOME%\lib\jetty-http-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-io-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-xml-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-servlet-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-util-9.0.2.v20130417.jar;%APP_HOME%\lib\jetty-security-9.0.2.v20130417.jar
 
 @rem Execute Dictionary-java
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %DICTIONARY_JAVA_OPTS%  -classpath "%CLASSPATH%" App %CMD_LINE_ARGS%
